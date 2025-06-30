6b66cd24be8480f7c0870f4f2b86c07671879e0d
Vinicius Borges <viniciussobreiraborges@hotmail.com>
2025-06-30 00:06:44 -0300

    corrigido erro de build

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import { Container } from '@/components/ui/container';
 import { PropertyData } from '@/types/special';
--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,5 +1,4 @@
-
-
+"use client";
 import React, { useState } from 'react';
 import { Container } from '@/components/ui/container';
 import { PropertyData } from '@/types/special';
